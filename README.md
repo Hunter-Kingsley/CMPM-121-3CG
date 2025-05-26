938bdf742c80b6f9ed80bf4e39c39062b75fa95f
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-05-26 10:22:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# 3CG (Casual Collectible Card Game)
---
+++ main.lua
@@ -0,0 +1,18 @@
+
+-- 3CG Game by Hunter Kingsley
+
+io.stdout:setvbuf("no")
+
+debug = true
+
+function love.load()
+  
+end
+
+function love.update()
+  
+end
+
+function love.draw()
+  
+end
