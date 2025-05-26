0c993214f79bdef8547cc7d8266f7a0195b4f030
Hunter-Kingsley <hunterkingsley329@gmail.com>
2025-05-26 10:32:19 -0700

    window setup

--- main.lua
+++ main.lua
@@ -6,7 +6,10 @@ io.stdout:setvbuf("no")
 debug = true
 
 function love.load()
-  
+  love.window.setMode(1920, 1080)
+  love.window.setTitle("Greek Style 3CG")
+  love.graphics.setBackgroundColor(0.4, 0.4, 0.4, 1)
+  love.graphics.setDefaultFilter("nearest", "nearest")
 end
 
 function love.update()
